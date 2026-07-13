a2a547250d9ae8d17a42609a24f841b8c20408bf
andrew-solarstorm <hoangdao.dev@gmail.com>
2026-07-13 14:17:35 +0700

    refactor: hardcoded account notification sync on startup

--- yellowstone-grpc-geyser/src/plugin/entry.rs
+++ yellowstone-grpc-geyser/src/plugin/entry.rs
@@ -354,7 +354,7 @@ impl GeyserPlugin for Plugin {
     }
 
     fn account_data_snapshot_notifications_enabled(&self) -> bool {
-        false
+        true
     }
 
     fn transaction_notifications_enabled(&self) -> bool {
